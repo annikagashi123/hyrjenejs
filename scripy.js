2b06d0d1f9ae3a835ccbb7c7143d75342e822211
annikagashi123 <annikagashi18@gmail.com>
2024-11-18 09:41:44 -0800

    mbarova

---
+++ scripy.js
@@ -0,0 +1,17 @@
+//this is a one line coment
+
+
+//variablat dhe datatypes:
+let emri = "mirjeta" // string
+let notaMatematike = 4; //number
+let x; // undefined
+let isAdult = false; //ture //boolean
+let nullVariable = null;
+
+//deklarimi i variable = krijojme nje variabel
+let testVar = "m";
+
+//deklarim + incializmi ne te njejtin rresht
+const PI = 3.14;
+
+// case sensitive:
